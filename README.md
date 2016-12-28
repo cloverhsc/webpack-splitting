5d1f8c9197c3da683d18aa7be23aa65213fb0a48
clover <clover@solidyear.com.tw>
2016-12-28 23:13:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Try webpack splitting by CommonsChunkPlugin
+
+### use chunk1.js chunk2.js and jquery to try CommonsChinkPlugin.
+
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+
+const PATHS = {
+    app: path.join(__dirname, "app"),
+    build: path.join(__dirname, "build")
+}
+
+module.exports = {
+    entry: {
+        main: './app/main.js',
+        main1: './app/main1.js',
+        chunk: [ "./app/chunk1", "./app/chunk2"],
+        vendor: ['jquery'],
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack try splitting'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ["vendor", "chunk"],
+            // filename: "chunk.js"
+            minChunk: 2,
+        })
+    ]
+};
